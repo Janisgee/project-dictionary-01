ad718ae20d72fb8a7cfb47e775175d18b206f17b
janisgee <janisgeegee@gmail.com>
2024-01-19 17:36:36 +0800

    Fixed photo display api bug

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Results from "./Results";
-import Photos from "./Photos";
+import React, { useState } from 'react';
+import { createClient } from 'pexels';
+import axios from 'axios';
+import Results from './Results';
+import Photos from './Photos';
 
 export default function Dictionary(props) {
   const [word, setWord] = useState(props.defaultWord);
@@ -13,11 +14,18 @@ export default function Dictionary(props) {
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
 
-    const pexelApiKey = `563492ad6f91700001000001fe893239473d4c79bb4b9aa25aaf56d0`;
-    const pexelUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=9`;
-    let headers = { Authorization: `Bearer ${pexelApiKey}` };
+    const client = createClient(
+      '563492ad6f91700001000001fe893239473d4c79bb4b9aa25aaf56d0',
+    );
 
-    axios.get(pexelUrl, { headers: headers }).then(handlePexelsResponse);
+    const query = setWord;
+    client.photos.search({ query, per_page: 9 }).then(handlePexelsResponse);
+
+    // const pexelApiKey = `563492ad6f91700001000001fe893239473d4c79bb4b9aa25aaf56d0`;
+    // const pexelUrl = `https://api.pexels.com/v1/search?query=${searchWord}&per_page=9`;
+    // let headers = { Authorization: `Bearer ${pexelApiKey}` };
+
+    // axios.get(pexelUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -39,24 +47,25 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
   }
   function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
+    console.log(response);
+    setPhotos(response.photos);
   }
   if (loaded) {
     return (
-      <div className="Dictionary">
-        <form className="form" onSubmit={handleSubmit}>
+      <div className='Dictionary'>
+        <form className='form' onSubmit={handleSubmit}>
           <input
-            type="search"
-            placeholder="Search a word ..."
-            className="searchForm"
+            type='search'
+            placeholder='Search a word ...'
+            className='searchForm'
             autoFocus={true}
             value={word}
             onChange={getInputWord}
           />
           <input
-            type="submit"
-            value="Search"
-            className="searchButton btn btn-warning"
+            type='submit'
+            value='Search'
+            className='searchButton btn btn-warning'
           />
         </form>
         <Results results={results} searchSynonymWord={searchSynonymWord} />
@@ -65,6 +74,6 @@ export default function Dictionary(props) {
     );
   } else {
     load();
-    return "Loading ...";
+    return 'Loading ...';
   }
 }
